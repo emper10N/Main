a90d97fc2a088ba1190e7c8312af82b436f6a309
emper10N <82743787+emper10N@users.noreply.github.com>
2023-11-26 00:27:57 +0500

    add a Apache POI

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -1,4 +1,27 @@
 package parser;
 
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.function.Consumer;
+
 public class Parser {
+    private final File templateFile;
+
+    public Parser(File templateFile) {
+        this.templateFile = templateFile;
+    }
+
+    public void convert(File outputFile, Consumer<Workbook> workbookComposer) throws IOException {
+        Files.copy(templateFile.toPath(), outputFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        try (Workbook workbook = new XSSFWorkbook(new FileInputStream(outputFile))) {
+            workbookComposer.accept(workbook);
+            try (OutputStream outputStream = new BufferedOutputStream(Files.newOutputStream(outputFile.toPath()))) {
+                workbook.write(outputStream);
+            }
+        }
+    }
 }
