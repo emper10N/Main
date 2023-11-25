cbee03eae4fe6f4a41590061cc3e54918eb467fd
emper10N <82743787+emper10N@users.noreply.github.com>
2023-11-26 04:47:09 +0500

    realise excel parsing

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -1,27 +1,52 @@
 package parser;
 
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
-import java.util.function.Consumer;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
 
 public class Parser {
-    private final File templateFile;
-
-    public Parser(File templateFile) {
-        this.templateFile = templateFile;
-    }
-
-    public void convert(File outputFile, Consumer<Workbook> workbookComposer) throws IOException {
-        Files.copy(templateFile.toPath(), outputFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-        try (Workbook workbook = new XSSFWorkbook(new FileInputStream(outputFile))) {
-            workbookComposer.accept(workbook);
-            try (OutputStream outputStream = new BufferedOutputStream(Files.newOutputStream(outputFile.toPath()))) {
-                workbook.write(outputStream);
-            }
+    public static String parse(String filename) {
+        String result = "";
+        InputStream inputStream = null;
+        XSSFWorkbook workBook = null;
+        try {
+            inputStream = new FileInputStream(filename);
+            workBook = new XSSFWorkbook(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        XSSFSheet sheet = workBook.getSheetAt(0);
+        Iterator<Row> it = sheet.iterator();
+        while (it.hasNext()) {
+            Row row = it.next();
+            Iterator<Cell> cells = row.iterator();
+            while (it.hasNext()) {
+                Cell cell = cells.next();
+                CellType cellType = cell.getCellType();
+                switch (cellType) {
+                    case STRING:
+                        result += cell.getStringCellValue() + " = ";
+                        break;
+                    case NUMERIC:
+                        result += " [" + cell.getNumericCellValue() + "] ";
+                        break;
+                    case FORMULA:
+                        result += " [" + cell.getCellFormula() + "] ";
+                    default:
+                        result += " | ";
+                        break;
+                }
+            }
+            result += "\n";
+        }
+
+        return result;
     }
 }
